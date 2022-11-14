d7143b4b678c5ec5bd22793b0c3062b3c5817eb1
David A. Ferguson <davferx@outlook.com>
2022-11-14 09:19:25 -0800

    build clang

--- compilers.md
+++
@@ -1,6 +0,0 @@
-C:\rtools40\mingw32\bin\...<br>
-Starting with R 4.0.0 (released April 2020), R for Windows uses a brand new toolchain bundle called rtools40.
-
-C:\tools\ghc-9.4.2\mingw\bin\...<br>
-GHC 9.4.2<br>
-GHC is a state-of-the-art, open source, compiler and interactive environment for the functional language Haskell.
--- tasks.ps1
+++ tasks.ps1
@@ -1,7 +1,7 @@
 # Task to be run during the build process
 param(
     [Parameter(Mandatory = $True)]
-    [ValidateSet('build', 'clean', 'cbuild', 'scan', 'list')]
+    [ValidateSet('build', 'clean', 'cbuild')]
     [string]$Cmd
 )
 
@@ -21,63 +21,8 @@ function DoClean {
     remove-item out -Recurse -Force -ErrorAction Ignore
 }
 
-function DoScan {
-    Write-Output 'Environment'
-    Write-Output '-----------'
-    cmd.exe /c set
-
-    Write-Output ''
-    Write-Output 'Path'
-    Write-Output '-----'
-    $env:Path.split(';')
-
-    Write-Output ''
-    Write-Output 'Root Dir C'
-    Write-Output  '----------'
-    cmd.exe /c dir c:\ /ogn
-
-    Write-Output ''
-    Write-Output 'Root Dir D'
-    Write-Output '----------'
-    cmd.exe /c dir d:\ /ogn
-
-    Write-Output ''
-    Write-Output 'Root Dir E'
-    Write-Output '----------'
-    cmd.exe /c dir d:\ /ogn
-
-    Write-Output ''
-    Write-Output 'Root Dir F'
-    Write-Output '----------'
-    cmd.exe /c dir f:\ /ogn
-
-    Write-Output ''
-    Write-Output 'Program Files'
-    Write-Output '-------------'
-    cmd.exe /c dir "c:\Program Files" /ogn
-
-    Write-Output ''
-    Write-Output 'Program Files (x86)'
-    Write-Output '-------------------'
-    cmd.exe /c dir "c:\Program Files (x86)" /ogn
-
-    Write-Output ''
-    Write-Output 'msys64'
-    Write-Output '-------------'
-    cmd.exe /c dir "c:\msys64" /ogn
-
-    # Write-Output ''
-    # Write-Output 'Compilers'
-    # Write-Output '---------'
-    # cmd.exe /c "dir c:\*.exe /s/b | grep -iE ""[\](cl|clang\+\+|cmake|gcc|ninja)\.exe$"""
-}
-
 switch ($Cmd.ToLower()) {
     'build' { DoBuild }
     'cbuild' { DoCBuild }
     'clean' { DoClean }
-    'scan' {
-        mkdir out\artifact -ErrorAction Ignore | Out-Null
-        DoScan >out\artifact\scan.log
-    }
 }
